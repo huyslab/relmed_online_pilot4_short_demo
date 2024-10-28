4c321a69bb7b722e87a53fea16fbf462986b430c
yanivabir <yanivabir@gmail.com>
2024-10-28 14:48:57 +0000

    don't save

--- utils.js
+++ utils.js
@@ -91,55 +91,9 @@ function check_fullscreen(){
 // Save data to REDCap
 function saveDataREDCap(retry = 1, callback = () => {}) {
 
-    const auto_number = window.record_id == undefined
+    console.log(jsPsych.date.get().values());
 
-    console.log(auto_number)
-
-    var jspsych_data = jsPsych.data.get().ignore('stimulus').json();
-
-    var redcap_record = JSON.stringify([{
-        record_id: auto_number ? 1 : window.record_id, // Mandatory, but if auto_number then ignored by REDcap
-        prolific_pid: window.prolificPID,
-        study_id: window.studyId,
-        session_id: window.sessionId,
-        start_time: jsPsych.getStartTime(),
-        jspsych_data: jspsych_data,
-        auto_number: auto_number ? 'true' : 'false'
-    }])
-
-    fetch('https://h6pgstm0f9.execute-api.eu-north-1.amazonaws.com/prod/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: redcap_record
-    })
-    .then(data => {
-        if (data.status === 200) {
-            console.log('Data successfully submitted to REDCap');
-        } else {
-            console.error('Error submitting data:', data.message);
-        }
-        return data.json()
-    })
-    .then(data => {
-        console.log(data)
-        if (auto_number){
-            window.record_id = JSON.parse('[' + data.record_import_response[0] + ']')[0]
-        }
-        callback(); // Call the callback function if submission is successful
-    }
-    )
-    .catch(error => {
-        console.error('Error:', error);
-        if (retry > 0) {
-            console.log('Retrying to submit data...');
-            saveDataREDCap(retry - 1);
-        } else {
-            console.error('Failed to submit data after retrying.');
-            callback(error); // Call the callback function with the error if retries are exhausted
-        }
-    });
+    callback();
 }
 
 // Function to call at the end of the experiment
